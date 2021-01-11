2e49ee0fe7e0a4574e5f323f8e7d8dcfc27fdcc8
viktormoitinho13 <viktormoitinho13@gmail.com>
2021-01-10 21:36:29 -0300

    Criação da página de exclusão de registros

---
+++ excluirRegistro.php
@@ -0,0 +1,16 @@
+<?php
+      include_once 'conexao.php';
+
+    $cod = $_GET['id']; // irá receber o id do registro a ser excluir por meio get(url)
+
+    $excluir = "delete from registro where id = '$cod'"; // cria uma variavel que terá o comando de exclusão de um registro(esse registro está dentro da variável $cod)
+    $executar = mysqli_query($conn,$excluir); // cria uma variavel($executar) que executa uma busca no banco de dados, buscando os dados da conexão ($conn) e fazendo o comando especificado na variavel acima ($excluir)
+
+    if($executar)
+        {
+            echo "registro apagado com sucesso";
+            echo "<br>";
+            echo "    <br/><button><a href ='index.php'>Voltar a página inicial</a> </button>";
+        }
+
+?>
